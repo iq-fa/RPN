51433216a9a22aca7548040dc487642a3a5a25fc
Iqfa <faiqhafidzardia@gmail.com>
2023-09-25 12:34:12 +0700

    upload kedua

---
+++ Part5.5.js
@@ -0,0 +1,56 @@
+//1. Let's Form a Sentence
+
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan penambahan string menggunakan simbol +.
+//  Disediakan variable word. Tambahkan nilai word satu per satu dengan nilai variable lain
+//  untuk membentuk sebuah kalimat. Jangan lupa menambahkan spasi di setiap kata, dan
+//   tampilkan di console hasil penggabungannya! Kamu tidak perlu membuat variable baru!
+
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+//code here
+
+
+// ---------------------------------------
+console.log(word+" "+second+" "+third+" "+fourth+" "+fifth+" "+sixth+" "+seventh);
+// ---------------------------------------
+
+// 2. Index Accessing - 1 by 1
+// Problem
+// Pada tugas ini kamu diminta untuk "memecah" sebuah kalimat dan menampilkan setiap kata didalamnya.
+//  Untuk soal nomor ini, gunakan akses satu per satu karakter dari string untuk mengambil setiap huruf dalam kata.
+//   Terasa manual? Tidak apa-apa, kita coba ini dulu untuk saat ini.
+
+// Hints
+// Saat kamu mendapatkan tiap huruf, untuk membentuk setiap kata kamu tinggal menggunakan simbol + untuk membentuk kata
+//  tersebut!
+
+word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2];
+
+console.log('First Word: ' + exampleFirstWord);
+
+
+// ---------------------------------------
+console.log(word[0]+word[1]+word[2]+word[3]+word[4]+word[5]+word[6]+word[7]+word[8]+word[9]+word[10]+word[11]+word[12]+word[13]+word[14]+word[15]+word[16]+word[17]+word[18]+word[19]+word[20]+word[21]+word[22]+word[23]+word[24]);
+// ---------------------------------------
+
+// 3. Breaking Sentence (Again) using Substring
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log('First Word: ' + exampleFirstWord3);
+
+
+// ---------------------------------------
+console.log('second Word: ' + word3.substring(4,15));
+// ---------------------------------------
