53256bc65f8c141c15737dd0a8b73c5ccb6e735c
Iqfa <faiqhafidzardia@gmail.com>
2023-10-04 15:14:55 +0700

    ramudeng nulis opo wkkww

--- 105.js
+++ 105.js
@@ -139,6 +139,11 @@ Contoh:
 
 function makanTerusRekursif(waktu) {
     // you can only write your code here!
+    if(waktu <1){
+        return 0
+    } else {
+        return 1 + makanTerusRekursif(waktu-15)
+    }
   }
   
   // TEST CASES
@@ -147,3 +152,71 @@ function makanTerusRekursif(waktu) {
   console.log(makanTerusRekursif(90)); // 6
   console.log(makanTerusRekursif(10)); // 1
   console.log(makanTerusRekursif(0)); // 0
+
+
+  console.log("soal 4")
+
+  /*
+Diberikan sebuah function totalDigitRekursif(angka) yang menerima satu parameter berupa angka. Function akan me-return nilai total dari digit tersebut dengan menambahkan satu per satu angka dari digit paling depan ke paling belakang. Wajib menggunakan rekursif untuk berlatih rekursif! Disarankan untuk tidak menggunakan looping for untuk soal ini.
+
+Contoh:
+
+512, berarti outputnya adalah 8, karena 5 + 1 + 2 = 8. 1542, berarti outputnya adalah 12, karena 1 + 5 + 4 + 2 = 12.
+*/
+
+function totalDigitRekursif(angka) {
+    let huruf = angka.toString()
+    huruf = huruf.split('')
+    if(huruf.length==0){
+        return 0
+    } else {
+        // let plus = parseInt(huruf[huruf.length-1])
+        let poped = huruf.pop()
+        // console.log(poped)
+        huruf = huruf.join('')
+        return parseInt(poped) + totalDigitRekursif(huruf)
+    }
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(totalDigitRekursif(512)); // 8
+  console.log(totalDigitRekursif(1542)); // 12
+  console.log(totalDigitRekursif(5)); // 5
+  console.log(totalDigitRekursif(21)); // 3
+  console.log(totalDigitRekursif(11111)); // 5
+
+
+  console.log("soal 5")
+
+  /*
+Diberikan sebuah function kaliTerusRekursif(angka) yang menerima satu parameter berupa angka. Function akan memproses angka yang lebih dari satu digit menjadi satu digit dengan melakukan perkalian. Bila masih lebih dari satu digit, terus lakukan perkalian setiap digit-nya untuk pada akhirnya mendapatkan satu digit. Wajib menggunakan rekursif untuk berlatih rekursif, namun kamu boleh menggunakan looping juga disini.
+
+Contoh:
+
+3 => 3, karena sudah satu digit 24 => 8. Karena 24 dua digit, maka kita lakukan 2 * 4 = 8. 8 satu digit, maka hasilnya adalah 8. 654 => 0. Karena 654 tiga digit, maka lakukan 6 * 5 * 4 = 120. 120 tiga digit, maka lakukan 1 * 2 * 0 = 0. 0 satu digit, maka hasilnya adalah 0.
+*/
+
+function kaliTerusRekursif(angka) {
+    // you can only write your code here!
+    // console.log(angka)
+    let huruf = angka.toString()
+    huruf = huruf.split('')
+    if(huruf.length == 1){
+        return angka
+    } else {
+        let kali = 1
+        for (let h=0;h<huruf.length;h++){
+            kali *=huruf[h]
+        }
+        // console.log(kali)
+        return kaliTerusRekursif(kali)
+    }
+  }
+  
+  // TEST CASES
+  console.log(kaliTerusRekursif(66)); // 8
+  console.log(kaliTerusRekursif(3)); // 3
+  console.log(kaliTerusRekursif(24)); // 8
+  console.log(kaliTerusRekursif(654)); // 0
+  console.log(kaliTerusRekursif(1231)); // 6
