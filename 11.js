ed4c31c9c21fb84dceb3637a834debe6a5b7094e
Iqfa <faiqhafidzardia@gmail.com>
2023-10-04 15:26:19 +0700

    new branch

---
+++ 11.js
@@ -0,0 +1,67 @@
+/*
+Diberikan function attack(), damageCalculation().
+
+Function damageCalculation() akan menerima 2 parameter yaitu numberOfAttacks dan damagePerAttack
+Function attack() akan menerima 1 parameter yaitu damage
+[IMPLEMENTASI] Gunakan function damageCalculation() untuk menghitung damage yang diterima dan gunakan function attack() untuk mensimulasikan setiap attack dengan rumus damage - 2 disetiap attack.
+
+[CONTOH] damageCalculation(3, 10) akan mengembalikan nilai 24
+
+Karena attack akan dikurangi 2, maka setiap attack akan menghasilkan damage 8
+*/
+
+function attack (damage) {
+    return damage - 2
+    // Code disini
+  }
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+    return numberOfAttacks* attack(damagePerAttack)
+    // Code disini
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
+
+  console.log("soal 2")
+  /*
+Diberikan function meleeRangedGrouping yang menerima 1 parameter berupa string, implementasikan meleeRangedGrouping agar dapat menghasilkan multidimensional array seperti yang diminta.
+
+Format string yang diberikan adalah: <nama_hero>-<tipe_hero>,<nama_hero>-<tipe-hero>, ...
+
+Output yang diharapkan: [ [ <daftar_hero_dengan_tipe_ranged> ], [ <daftar_hero_dengan_tipe_melee> ] ]
+
+Jika input adalah string kosong ('') maka return array kosong
+*/
+
+function meleeRangedGrouping (str) {
+    if(str==''){
+        return []
+    }
+    let grup = [[],[]]
+    let hero = str.split(',')
+    for(let h=0;h<hero.length;h++){
+        let split = hero[h].split('-')
+        if (split[1]=='Ranged'){
+            grup[0].push(split[0])
+        } else {
+            grup[1].push(split[0])
+        }
+    }
+    return grup
+    //your code here
+  }
+  
+  // TEST CASE
+  
+  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+  
+  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+  // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+  
+  console.log(meleeRangedGrouping('')); // []
